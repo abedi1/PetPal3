65b85e2617a90a0b93fc18497476f88642aa2500
AmanB <asbedi@outlook.com>
2022-10-25 14:42:04 -0400

    update that I know works

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+// import Icon from 'react-native-vector-icons/FontAwesome'
 const App = () => {
+  const color = '#b5b5b5';
   return (
     <GestureHandlerRootView style={styles.pageContainer}>
-      <MatchesScreen/>
+      <HomeScreen/>
     </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
   },
 });
 
