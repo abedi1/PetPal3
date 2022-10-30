f8c633cdfd3fdeba6475b7fc7ec0616af4743f00
AmanB <asbedi@outlook.com>
2022-10-30 15:09:10 -0400

    added navigation and top and bottom icons.

--- App.js
+++ App.js
@@ -1,17 +1,60 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Pressable, View} from 'react-native';
 
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-// import Icon from 'react-native-vector-icons/FontAwesome'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 const App = () => {
   const color = '#b5b5b5';
+  const activeColor = '#F76C6B';
+  const topIconSize = 30;
+  const [activeScreen, setActiveScreen] = useState('HOME');
   return (
-    <GestureHandlerRootView style={styles.pageContainer}>
-      <HomeScreen/>
-    </GestureHandlerRootView>
+    <SafeAreaView style={styles.root}>
+      <GestureHandlerRootView style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <MaterialIcons
+              name="pets"
+              size={topIconSize}
+              color={activeScreen === 'HOME' ? activeColor : color}
+            />
+          </Pressable>
+
+          <Pressable onPress={() => setActiveScreen('LIKES')}>
+            <MaterialCommunityIcons
+              name="star-four-points"
+              size={topIconSize}
+              color={activeScreen === 'LIKES' ? activeColor : color}
+            />
+          </Pressable>
+
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons
+              name="ios-chatbubbles"
+              size={topIconSize}
+              color={activeScreen === 'CHAT' ? activeColor : color}
+            />
+          </Pressable>
+
+          <Pressable onPress={() => setActiveScreen('USER')}>
+            <FontAwesome
+              name="user"
+              size={topIconSize}
+              color={activeScreen === 'USER' ? activeColor : color}
+            />
+          </Pressable>
+        </View>
+        {activeScreen === 'HOME' && <HomeScreen />}
+        {activeScreen === 'CHAT' && <MatchesScreen />}
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 };
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,9 @@ import Card from '../components/TinderCard';
 import users from '../../assets/data/animals';
 
 import AnimatedStack from '../components/AnimatedStack';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const HomeScreen = () => {
   const onSwipeLeft = user => {
@@ -13,6 +16,7 @@ const HomeScreen = () => {
   const onSwipeRight = user => {
     console.warn('swipe right: ', user.name);
   };
+  const bottomIconSize = 30;
 
   return (
     <View style={styles.pageContainer}>
@@ -22,6 +26,23 @@ const HomeScreen = () => {
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
       />
+      <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={bottomIconSize} color="#FBD88B" />
+        </View>
+        <View style={styles.button}>
+          <Entypo name="cross" size={bottomIconSize} color="#F76C6B" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="star" size={bottomIconSize} color="#3AB4CC" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={bottomIconSize} color="#4FCC94" />
+        </View>
+        <View style={styles.button}>
+          <Ionicons name="flash" size={bottomIconSize} color="#A65CD2" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -32,6 +53,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     width: '100%',
+    backgroundColor: '#ededed',
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
   },
 });
 
