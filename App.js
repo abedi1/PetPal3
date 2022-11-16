e13cd2ecba7ee838bfe76beedd8d1e1c92d2360b
annierhoades <78170175+annierhoades@users.noreply.github.com>
2022-11-15 23:30:34 -0500

    change to show initial screen
    -this now displays the initial screen i made BUT the buttons don't work right now 
    -trying to figure out how to get "renderPage" to switch the screen only AFTER a button is pushed
    -original App.js code is now in "screens/SignTest.js" (because im testing sign in options)

--- App.js
+++ App.js
@@ -1,187 +1,150 @@
-import 'react-native-gesture-handler';
-import '@azure/core-asynciterator-polyfill';
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, StyleSheet, Pressable, View, ActivityIndicator} from 'react-native';
-
+import {SafeAreaView, Text, View, Image, Button, TouchableOpacity, Alert, StyleSheet, ActivityIndicator} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import InitScreen from './src/screens/InitScreen';
+import SignInScreen from './src/screens/SignInScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {Amplify, Hub} from 'aws-amplify';
-import {withAuthenticator, AmplifyTheme} from 'aws-amplify-react-native';
-import awsconfig from './src/aws-exports';
 import ProfileScreen from './src/screens/ProfileScreen';
+import SignTest from './src/screens/SignTest';
 
-Amplify.configure({
-  awsconfig,
-  Analytics: {
-    disabled: true,
-  },
-});
+
+  
 
 const App = () => {
-  const color = '#b5b5b5';
-  const activeColor = '#f07c3c';
-  const topIconSize = 30;
-  const [activeScreen, setActiveScreen] = useState('HOME');
+
+  const [activeScreen, setActiveScreen] = useState('INIT');
   const [isUserLoading, setIsUserLoading] = useState(true)
 
-  useEffect(() => {
-    const listener = Hub.listen('datastore', async hubData => {
-      const {event, data} = hubData.payload;
-      if (event === 'modelSynced' && data?.model?.name === 'User') {
-        console.log('User Model has finished syncing');
-        setIsUserLoading(false);
-      }
-    });
+  const renderPage = () =>{
+    if (activeScreen === 'INIT') {
+      return <InitScreen/>
+    }
 
-    return () => listener(); //removes listener when apllication closes
-  }, []);
+    if (activeScreen === 'SIGNIN') {
+      return <SignInScreen isUserLoading={isUserLoading}/>
+    }
+  
+    if (activeScreen === 'TEST'){
+      return <SignTest isUserLoading={isUserLoading}/>
+    }
 
- const renderPage = () =>{
-  if (activeScreen === 'HOME'){
-    return <HomeScreen isUserLoading={isUserLoading}/>
-  }
+    if (activeScreen === 'HOME'){
+      return <HomeScreen isUserLoading={isUserLoading}/>
+    }
+  
+    if (isUserLoading){
+      return <ActivityIndicator style ={{flex:1}}/>
+    }
+  
+    if (activeScreen === 'CHAT'){
+      return <MatchesScreen/>
+    }
+    if (activeScreen === 'PROFILE'){
+      return <ProfileScreen/>
+    }
+   }
 
-  if (isUserLoading){
-    return <ActivityIndicator style ={{flex:1}}/>
-  }
-
-  if (activeScreen === 'CHAT'){
-    return <MatchesScreen/>
-  }
-  if (activeScreen === 'PROFILE'){
-    return <ProfileScreen/>
-  }
- }
-
-  return (
-    <SafeAreaView style={styles.root}>
-      <GestureHandlerRootView style={styles.pageContainer}>
-        <View style={styles.topNavigation}>
-          <Pressable onPress={() => setActiveScreen('HOME')}>
-            <MaterialIcons
-              name="pets"
-              size={topIconSize}
-              color={activeScreen === 'HOME' ? activeColor : color}
+   
+    return (
+      <View style={styles.container}>
+            <Text style={styles.main}>
+                PetPal</Text>
+            <Image
+                source={{
+                    uri: 'https://cdn.pixabay.com/photo/2016/04/07/18/57/silhouette-1314467__340.png',  
+                }}
+                style={styles.img}
             />
-          </Pressable>
-
-          <Pressable onPress={() => setActiveScreen('LIKES')}>
-            <MaterialCommunityIcons
-              name="star-four-points"
-              size={topIconSize}
-              color={activeScreen === 'LIKES' ? activeColor : color}
-            />
-          </Pressable>
-
-          <Pressable onPress={() => setActiveScreen('CHAT')}>
-            <Ionicons
-              name="ios-chatbubbles"
-              size={topIconSize}
-              color={activeScreen === 'CHAT' ? activeColor : color}
-            />
-          </Pressable>
-
-          <Pressable onPress={() => setActiveScreen('PROFILE')}>
-            <FontAwesome
-              name="user"
-              size={topIconSize}
-              color={activeScreen === 'PROFILE' ? activeColor : color}
-            />
-          </Pressable>
+            <Text style={styles.smallText}>By creating an account or logging in, you are agreeing to our extensive Terms of Service and Privacy Policy</Text>
+            <TouchableOpacity style={styles.buttonz1} onPress={() => setActiveScreen('TEST')}>
+                <Text style={styles.buttonText1}>SIGN UP</Text>
+             </TouchableOpacity>
+             <TouchableOpacity style={styles.buttonz2} onPress={() => setActiveScreen('TEST')}>
+                <Text style={styles.buttonText2}>SIGN IN</Text>
+             </TouchableOpacity>
+             <TouchableOpacity style={styles.forgot} onPress={() => setActiveScreen('TEST')}>
+                <Text style={{fontSize: 15, color: 'white',}}>Forgot your password?</Text>
+             </TouchableOpacity>
+             
         </View>
-
-        {renderPage()}
-
-      </GestureHandlerRootView>
-    </SafeAreaView>
-  );
-};
+       //{renderPage()}
+    );
+}
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#e97a3a',
+
+
+    },
+    main: {
+        fontFamily: "Gill Sans",
+        fontSize: 45,
+        fontWeight: "bold",
+        color: 'white',
+        marginBottom: 8
+        
+    },
+    buttonText1: {
+        textAlign: 'center',
+        fontFamily: "Gill Sans",
+        fontSize: 20,
+        color: 'white',
+        paddingHorizontal:60,
+        paddingVertical:4
+        
+    },
+    buttonText2: {
+      textAlign: 'center',
+      fontFamily: "Gill Sans",
+      fontSize: 20,
+      color: 'white',
+      paddingHorizontal:60,
+      paddingVertical:4
+      
   },
-  pageContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-  topNavigation: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-    padding: 10,
-  },
-});
+    buttonz1: {
+        backgroundColor:'#e97a3a',
+        borderRadius: 20,
+        borderWidth: 2,
+        borderStyle: 'solid',
+        color: 'black',
+        overflow:'hidden',
+        underlayColor: 'black',
+        marginBottom: 8,
+        width: 210
+    },
+    buttonz2: {
+        backgroundColor:'#e97a3a',
+        borderRadius: 20,
+        borderWidth: 2,
+        borderStyle: 'solid',
+        color: 'black',
+        overflow:'hidden',
+        underlayColor: 'black',
+        marginBottom: 8,
+        width: 250
+    },
+    forgot: {
+        backgroundColor:'#e97a3a',
+    },
+    img: {
+        alignSelf: 'center',
+        width: 110, 
+        height: 110,
+        marginBottom: 175,
+    },
+    smallText: {
+        fontSize: 10, 
+        color: 'white',
+        textAlign: 'center',
+        marginBottom: 8,
+        marginHorizontal:10
+    }
+  });
 
-const signUpConfig = {
-  header: "SIGN UP",
-  hideAllDefaults: true,
-  signUpFields: [
-    {
-      label: "Username",
-      key: "preferred_username",
-      required: true,
-      displayOrder: 1,
-      type: "string",
-    },
-    {
-      label: "Password",
-      key: "password",
-      required: true,
-      displayOrder: 2,
-      type: "password",
-    },
-    {
-      label: "Email",
-      key: "email",
-      required: true,
-      displayOrder: 3,
-      type: "string",
-    },
-    
-  ],
-};
-
-const signInConfig = {
-  header: "SIGN IN",
-  hideAllDefaults: true,
-  signInFields: [
-    {
-      label: "Username",
-      key: "preferred_username",
-      required: true,
-      displayOrder: 1,
-      type: "string",
-    },
-    {
-      label: "Password",
-      key: "password",
-      required: true,
-      displayOrder: 2,
-      type: "password",
-    },
-    
-  ],
-};
-
-const customTheme = {
-  ...AmplifyTheme,
-  sectionHeader: {
-		width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-		marginBottom: 32,
-		paddingTop: 20,
-	},
-  button: {
-    ...AmplifyTheme.button,
-  }
-};
-
-export default withAuthenticator(App, {signInConfig, signUpConfig, theme: customTheme});
+export default App;
