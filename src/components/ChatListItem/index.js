2d2abca2cbff6281cad3e93823d54200c131c2ff
EtnaOzkara <etnaozkara@gmail.com>
2022-11-27 17:38:47 -0500

    last

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -4,30 +4,35 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import {useNavigation} from '@react-navigation/native';
 dayjs.extend(relativeTime);
 
-
 const ChatListItem = ({chat}) => {
+  const navigation = useNavigation();
+
   return (
-      <View style={styles.container}>
-        {/* User Avatar */}
-        <Image source={{uri: chat.user.image}} style={styles.image} />
+    <Pressable
+      onPress={() =>
+        navigation.navigate('Chat', {id: chat.id, name: chat.user.name})
+      }
+      style={styles.container}>
+      {/* User Avatar */}
+      <Image source={{uri: chat.user.image}} style={styles.image} />
 
-        {/* content */}
-        <View style={styles.content}>
-          <View style={styles.row}>
-            <Text style={styles.name} numberOfLines={1}>
-              {chat.user.name}
-            </Text>
+      {/* content */}
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Text style={styles.name} numberOfLines={1}>
+            {chat.user.name}
+          </Text>
 
-            <Text style={styles.subTitle}>
-              {dayjs(chat.lastMessage.createdAt).fromNow()}
-            </Text>
-          </View>
-
-          <Text style={styles.subTitle} numberOfLines={2}>
-            {chat.lastMessage.text}
+          <Text style={styles.subTitle}>
+            {dayjs(chat.lastMessage.createdAt).fromNow()}
           </Text>
         </View>
+
+        <Text style={styles.subTitle} numberOfLines={2}>
+          {chat.lastMessage.text}
+        </Text>
       </View>
+    </Pressable>
   );
 };
 
