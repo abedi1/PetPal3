91779cf2b878ea08947f1f7bbd2328b259698e1e
annierhoades <78170175+annierhoades@users.noreply.github.com>
2022-11-30 10:14:33 -0500

    Update SignTest.js
    -formating edits

--- src/screens/SignTest.js
+++ src/screens/SignTest.js
@@ -10,10 +10,13 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {Amplify, Hub} from 'aws-amplify';
+import {Amplify, Hub, DataStore, Auth} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 import awsconfig from '../aws-exports';
 import ProfileScreen from './ProfileScreen';
+import {User} from '../models';
+
+
 
 Amplify.configure({
   awsconfig,
@@ -24,10 +27,12 @@ Amplify.configure({
 
 const SignTest = () => {
   const color = '#b5b5b5';
-  const activeColor = '#F76C6B';
+  const activeColor = 'black';
   const topIconSize = 30;
   const [activeScreen, setActiveScreen] = useState('HOME');
   const [isUserLoading, setIsUserLoading] = useState(true)
+  const [me, setMe] = useState(null);
+
 
   useEffect(() => {
     const listener = Hub.listen('datastore', async hubData => {
@@ -40,6 +45,24 @@ const SignTest = () => {
 
     return () => listener(); //removes listener when apllication closes
   }, []);
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      const user = await Auth.currentAuthenticatedUser();
+
+      const dbUsers = await DataStore.query(User, u =>
+        u.sub('eq', user.attributes.sub),
+      );
+      if (!dbUsers || dbUsers.length <= 0) {
+        setMe(null)
+        setActiveScreen('PROFILE'); // If they don't have an account make the default screen be profile.
+        return;
+      }
+      setMe(dbUsers[0]);
+    };
+    getCurrentUser();
+  }, [isUserLoading]);
+
 
  const renderPage = () =>{
   if (activeScreen === 'HOME'){
@@ -105,6 +128,8 @@ const SignTest = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+    flexDirection: 'row',
+    width: 350,
   },
   pageContainer: {
     justifyContent: 'center',
