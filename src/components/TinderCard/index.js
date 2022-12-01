e2d332a815fc62637b514d62b5c4430f66db1943
annierhoades <78170175+annierhoades@users.noreply.github.com>
2022-12-01 14:22:17 -0500

    Update index.js
    -formatting changes
    -text with gray background for visability + moved to under the card

--- src/components/TinderCard/index.js
+++ src/components/TinderCard/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, ImageBackground, View, StyleSheet} from 'react-native';
+import {Storage} from 'aws-amplify';
+import { set } from 'react-native-reanimated';
 
 const Card = props => {
   const {name, image, bio} = props.user;
+  const [imageUrl, setImageUrl] = useState(image);
+
+  useEffect(()=> {
+    if (!image?.startsWith('http')){
+      Storage.get(image).then(setImageUrl) // downloads the s3Image
+    } else{
+      setImageUrl(image);
+    }
+  }, [image])
+
+
   return (
     <View style={styles.card}>
       <ImageBackground
         source={{
-          uri: image,
+          uri: imageUrl,
         }}
         style={styles.image}>
-        <View style={styles.cardInner}>
+        
+      </ImageBackground>
+      <View style={styles.cardInner}>
           <Text style={styles.name}>{name}</Text>
           <Text style={styles.bio}>{bio}</Text>
         </View>
-      </ImageBackground>
     </View>
   );
 };
@@ -35,6 +49,7 @@ const styles = StyleSheet.create({
     shadowRadius: 6.68,
 
     elevation: 11,
+    marginBottom: 50,
   },
   image: {
     width: '100%',
@@ -45,12 +60,16 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
   cardInner: {
-    padding: 10,
+     //marginBottom: 350,
+   padding: 10,
+   backgroundColor: '#b5b5b5',
+   //opacity: 0.7,
   },
   name: {
     fontSize: 30,
     color: 'white',
     fontWeight: 'bold',
+    backgroundColor: '#b5b5b5',
   },
   bio: {
     fontSize: 18,
