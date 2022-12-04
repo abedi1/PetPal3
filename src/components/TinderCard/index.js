d148d0bb0da294e6804469a9b12d80b4c6e3adf9
annierhoades <78170175+annierhoades@users.noreply.github.com>
2022-12-04 13:07:07 -0500

    Update index.js
    added limit on how many lines of bio are shown
    added modal which shows the full bio when clicked

--- src/components/TinderCard/index.js
+++ src/components/TinderCard/index.js
@@ -1,11 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import {Text, ImageBackground, View, StyleSheet} from 'react-native';
+import {Text, ImageBackground, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
 import {Storage} from 'aws-amplify';
 import { set } from 'react-native-reanimated';
+import Modal from 'react-native-modal';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+const bottomIconSize = 30;
 
 const Card = props => {
   const {name, image, bio} = props.user;
   const [imageUrl, setImageUrl] = useState(image);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const handleModal = () => setIsModalVisible(() => !isModalVisible);
 
   useEffect(()=> {
     if (!image?.startsWith('http')){
@@ -18,6 +25,25 @@ const Card = props => {
 
   return (
     <View style={styles.card}>
+      <Modal
+        isVisible={isModalVisible}
+        animationInTiming={500}
+        animationOutTiming={500}
+        backdropTransitionInTiming={800}
+        backdropTransitionOutTiming={800}
+        onBackdropPress={handleModal}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalHeadingText}>{name}</Text>
+          </View>
+          <View style={styles.modalBody}>
+            <Text style={styles.fullbio}>{bio}</Text>
+          </View>
+          <View style = {styles.modalFooter}>
+            <Button title="Close" onPress={handleModal} color ={'#e97a3a'} borderRadius={100} />
+          </View>
+        </View>
+      </Modal>
       <ImageBackground
         source={{
           uri: imageUrl,
@@ -27,7 +53,12 @@ const Card = props => {
       </ImageBackground>
       <View style={styles.cardInner}>
           <Text style={styles.name}>{name}</Text>
-          <Text style={styles.bio}>{bio}</Text>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={handleModal}>
+              <Text style={{fontWeight: 'normal', fontSize: 30, color: '#b5b5b5'}}>+</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.bio} numberOfLines={2}>{bio}{'\n'}</Text>
         </View>
     </View>
   );
@@ -65,17 +96,77 @@ const styles = StyleSheet.create({
    backgroundColor: '#b5b5b5',
    //opacity: 0.7,
   },
+  button: {
+    width: '10%',
+    height: '40%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    //paddingHorizontal: 10,
+    borderRadius: 100,
+    opacity: 0.8,
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    opacity: 0.6,
+    
+  },
   name: {
     fontSize: 30,
     color: 'white',
     fontWeight: 'bold',
     backgroundColor: '#b5b5b5',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
   },
   bio: {
     fontSize: 18,
     color: 'white',
     lineHeight: 25,
-    backgroundColor:'black',
+    
+  },
+  fullbio: { // no max number 
+    fontSize: 18,
+    color: 'black',
+    lineHeight: 25,
+  },
+  modalContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderStyle: 'solid',
+  },
+  modalHeader: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: 'gray',
+    borderBottomWidth: 0.2,
+  },
+  modalHeadingText: {
+    paddingTop: 10,
+    textAlign: 'center',
+    fontFamily: "Gill Sans",
+    fontSize: 24,
+    fontWeight: 'bold',
+    
+  },
+  modalText: {
+    fontSize: 18,
+    
+  },
+  modalBody: {
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    minHeight: 100,
+    //borderBottomColor: '#e97a3a',
+    //borderBottomWidth: 0.2,
+    //paddingBottom: 10,
+  },
+  modalFooter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 3,
+    flexDirection: 'row',
   },
 });
 
